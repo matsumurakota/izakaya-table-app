6ad9dc428e571c5a5b9bf4215d25fc7049d45f08
matsumurakota <songcunhaotai@gmail.com>
2025-05-10 12:21:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 
-// PWA version of the Table Manager App
-// Developed using HTML, CSS, and JavaScript (Touch-optimized version)
-
-// ※ 最新の JavaScript コードは canvas 上の内容に基づいています
-
-// 以下に JavaScript の内容をペーストすることも可能
-// 今は canvas の内容を利用します
+// ここにcanvasのコード全文が自動的に入る（略）
+console.log("main.js loaded 正常動作確認用");
